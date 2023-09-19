7d506facdcff4e2b9b21c44d02d566a0af066283
Mark Carrington <frumarkcarrington@gmail.com>
2023-09-19 23:53:01 +0530

    program for date

---
+++ Date.cpp
@@ -0,0 +1,14 @@
+// this program is all about date programming
+#include <stdio.h>
+
+int getDaysOfMonth(int month){
+
+    int monthDays;
+    switch(month){
+        case
+    }
+    return monthDays;
+}
+int main(void){
+
+}
