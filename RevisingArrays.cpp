2aea2cd030e92696ab4547b86e354fd944c89b3c
Mark Carrington <frumarkcarrington@gmail.com>
2023-10-05 14:23:32 +0530

    using the simple functions of the array

--- RevisingArrays.cpp
+++ RevisingArrays.cpp
@@ -2,10 +2,21 @@
 
 
 void usingSimpleArrays(){
+    // method 1 in creating an array;
+    int numbers[5]; // declaring an array without initialising the values of the array
     
+    // filling the array using while loops
+
+    for (int i = 0; i<5; i++){
+        numbers[i] = i+1;
+    }
+    printf("This is the print test of the array\n");
+    for (int i = 0; i<5; i++){
+        printf("%d\t", i);
+    }
 }
 
 int main(void){
-
- return 0;   
+    usingSimpleArrays();
+    return 0;   
 }
